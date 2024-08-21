7c0da3e7d4f6a4aa676686cc4b7b6ebbe47e533d
DankChoir <timzek7@gmail.com>
2024-08-21 16:21:38 +0700

    Basic CRUD system

--- src/main/java/dev/danko/runnerz/Run/Run.java
+++ src/main/java/dev/danko/runnerz/Run/Run.java
@@ -1,11 +1,22 @@
 package dev.danko.runnerz.Run;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
 import java.time.LocalDateTime;
 
 public record Run(Integer id,
+                  @NotEmpty
                   String title,
                   LocalDateTime startedOn,
                   LocalDateTime completedOn,
+                  @Positive
                   Integer distance,
                   Location location) {
+
+    public Run {
+        if(startedOn.isAfter(completedOn)){
+            throw new IllegalArgumentException();
+        }
+    }
 }
--- src/main/java/dev/danko/runnerz/Run/RunController.java
+++ src/main/java/dev/danko/runnerz/Run/RunController.java
@@ -1,11 +1,12 @@
 package dev.danko.runnerz.Run;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/runs")
@@ -23,7 +24,34 @@ public class RunController {
     }
 
     @GetMapping("/{id}")
-    Run findById(@PathVariable Integer id){
-        return runRepository.findById(id);
+    Run findById(@PathVariable Integer id) {
+        Optional<Run> targetRun = runRepository.findById(id);
+
+        if (targetRun.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        return targetRun.get();
+    }
+
+    // post : create
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    void create(@Valid @RequestBody Run run) {
+        runRepository.create(run);
+    }
+
+    // put : edit
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    void put(@Valid @RequestBody Run run, @PathVariable Integer id) {
+        runRepository.update(run,id);
+    }
+
+    // delete
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    void delete(@Valid @PathVariable Integer id){
+        runRepository.delete(id);
     }
 }
